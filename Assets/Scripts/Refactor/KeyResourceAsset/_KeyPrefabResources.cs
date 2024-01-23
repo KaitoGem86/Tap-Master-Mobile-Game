472300eb4b17f9096b8e5299604ed004c9495cb1
TuanDat <letuandat556@gmail.com>
2024-01-23 09:19:51 +0700

    add addressables group settings

---
+++ Assets/Scripts/Refactor/KeyResourceAsset/_KeyPrefabResources.cs
@@ -0,0 +1,11 @@
+namespace Core.ResourceGamePlay{
+    public class _KeyPrefabResources{
+        private static string _keyCameraRotation = "CameraRotation";
+
+        public static string KeyCameraRotation{
+            get{
+                return _keyCameraRotation;
+            }
+        }
+    }
+}
