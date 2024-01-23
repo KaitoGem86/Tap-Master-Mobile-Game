fbde93b9bcc79cb8300d42cf9c9e4f91bb3e341a
TuanDat <letuandat556@gmail.com>
2024-01-23 09:37:08 +0700

    add dont destroy on load

--- Assets/Scripts/Refactor/GamePlay/_GamePlaySceneContext.cs
+++ Assets/Scripts/Refactor/GamePlay/_GamePlaySceneContext.cs
@@ -1,23 +1,42 @@
+using Core.ResourceGamePlay;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Core.GamePlay
 {
     public class _GamePlaySceneContext : MonoBehaviour
     {
+        private void Awake()
+        {
+            Init();
+            DontDestroyOnLoad();
+        }
+
         // Start is called before the first frame update
         void Start()
         {
-
+            SetUpCamera();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
+        private async void DontDestroyOnLoad(){
+            var dontDestroyOnLoad = await AddressablesManager.LoadAssetAsync<GameObject>(_KeyPrefabResources.KeyDontDestroyOnLoad);
+            var gameObject = GameObject.Instantiate(dontDestroyOnLoad);
+        }
+        
+        private void Init(){
+            DOTween.Init();
+            Application.targetFrameRate = 60;
         }
 
-        private void SetUpCamera(){
-            
+        private async void SetUpCamera(){
+            var cameraRotation = await AddressablesManager.LoadAssetAsync<GameObject>(_KeyPrefabResources.KeyCameraRotation);
+            var gameObject = GameObject.Instantiate(cameraRotation);
+#if UNITY_EDITOR
+            gameObject.name = cameraRotation.Value.name;
+#endif     
         }
+
+
     }
 }
--- Assets/Scripts/Refactor/KeyResourceAsset/_KeyPrefabResources.cs
+++ Assets/Scripts/Refactor/KeyResourceAsset/_KeyPrefabResources.cs
@@ -1,10 +1,18 @@
 namespace Core.ResourceGamePlay{
     public class _KeyPrefabResources{
         private static string _keyCameraRotation = "CameraRotation";
+        private static string _keyDontDestroyOnLoad = "DontDestroyOnLoad";
+
 
         public static string KeyCameraRotation{
             get{
                 return _keyCameraRotation;
+            }
+        }
+
+        public static string KeyDontDestroyOnLoad{
+            get{
+                return _keyDontDestroyOnLoad;
             }
         }
     }
