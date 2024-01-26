fae6c94d0f490795d5a452f03d4da503aeef8463
TuanDat <letuandat556@gmail.com>
2024-01-26 14:16:59 +0700

    stop create level

--- Assets/Scripts/Refactor/Generate/_GenerateLevel.cs
+++ Assets/Scripts/Refactor/Generate/_GenerateLevel.cs
@@ -10,6 +10,18 @@ namespace Generate{
                 Debug.Log(v);
                 Debug.DrawRay(v, Vector3.up*0.2f, Color.red, 100f);
             }
+
+            var t2 = _blockMesh.triangles;
+            for(int i = 0; i < t2.Length; i += 3){
+                // Debug.Log(v);
+
+                var pos1 = t[t2[i]];
+                var pos2 = t[t2[i + 1]];
+                var pos3 = t[t2[i + 2]];
+                Debug.DrawRay(pos2, pos1 - pos2, Color.blue, 100f);
+                Debug.DrawRay(pos3, pos2 - pos3, Color.blue, 100f);
+                Debug.DrawRay(pos1, pos3 - pos1, Color.blue, 100f);
+            }
         }
     }
 }
